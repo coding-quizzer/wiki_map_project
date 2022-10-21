07e940d7680bf47e1a00082c4fb602a153f2c4de
William Gadd <wgadd@sasktel.net>
2022-10-21 21:59:43 +0000

    Added users table with @kruciall

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (first_name, last_name, username, password) VALUES ('Vinson', 'Wong', 'kruciall', '12345');
+INSERT INTO users (first_name, last_name, username, password) VALUES ('William', 'Gadd', 'coding-quizzer', '54321');
+INSERT INTO users (first_name, last_name, username, password) VALUES ('Andrew', 'Smith', 'three', '12345');
+INSERT INTO users (first_name, last_name, username, password) VALUES ('Vasiliy', 'Kim', 'captwaffles', '12345');
+INSERT INTO users (first_name, last_name, username, password) VALUES ('Francis', 'Bourgouis', 'franny', '12345');
+

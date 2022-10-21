b0bc943a83d6fbe03ef5903299b4af3343985faf
William Gadd <wgadd@sasktel.net>
2022-10-21 21:18:05 +0000

    Add router for GET maps/:id path

--- routes/maps.js
+++ routes/maps.js
@@ -5,4 +5,10 @@ router.get('/', (req, res) => {
   res.render('maps');
 });
 
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  const templateVars = { id };
+  res.render('maps', templateVars);
+});
+
 module.exports = router;

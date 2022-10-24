237d151ba0d99a6e937c1d593634264317a8a95d
William Gadd <wgadd@sasktel.net>
2022-10-24 18:37:21 +0000

    Change column name from image to img_url for consistency and refactored seeds for points table into one insert query

--- db/seeds/03_points.sql
+++ db/seeds/03_points.sql
@@ -1,7 +1,8 @@
-INSERT INTO points (title, image, description, map_id, latitude, longitude) VALUES ('Tim Hortons', 'IMG', 'TIMMMIES', 1, 50.482456, -104.615591);
-INSERT INTO points (title, image, description, map_id, latitude, longitude) VALUES ('Starbucks', 'IMG', 'Go to Coffee place', 1, 43.856367, -79.337671);
-INSERT INTO points (title, image, description, map_id, latitude, longitude) VALUES ('Corner Cafe', 'IMG', 'Chill Vibes', 1, 50.458747, -104.648633);
-INSERT INTO points (title, image, description, map_id, latitude, longitude) VALUES ('Baskin Robbins', 'IMG', 'YUMYUM', 2, 49.341434, -123.156975);
-INSERT INTO points (title, image, description, map_id, latitude, longitude) VALUES ('ABC Theatres', 'IMG', 'IMAX Theatre', 3, 47.544195, -52.743841);
-INSERT INTO points (title, image, description, map_id, latitude, longitude) VALUES ('AMC Movie Theatres', 'IMG', 'AMC AMC AMC', 3, 42.983228, -81.24337);
-INSERT INTO points (title, image, description, map_id, latitude, longitude) VALUES ('Costco', 'IMG', 'Go to groceries', 4, 46.817119, -71.277264);
+INSERT INTO points (title, img_url, description, map_id, latitude, longitude)
+VALUES ('Tim Hortons', 'IMG', 'TIMMMIES', 1, 50.482456, -104.615591),
+('Starbucks', 'IMG', 'Go to Coffee place', 1, 43.856367, -79.337671),
+('Corner Cafe', 'IMG', 'Chill Vibes', 1, 50.458747, -104.648633),
+('Baskin Robbins', 'IMG', 'YUMYUM', 2, 49.341434, -123.156975),
+('ABC Theatres', 'IMG', 'IMAX Theatre', 3, 47.544195, -52.743841),
+('AMC Movie Theatres', 'IMG', 'AMC AMC AMC', 3, 42.983228, -81.24337),
+('Costco', 'IMG', 'Go to groceries', 4, 46.817119, -71.277264);

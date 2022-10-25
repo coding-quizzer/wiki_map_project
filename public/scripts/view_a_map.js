b93be6361651080b71826c0c7f3681c3cad962c5
William Gadd <wgadd@sasktel.net>
2022-10-25 15:42:02 +0000

    Added basic code neccesary to get leaflet-js up and running

--- public/scripts/view_a_map.js
+++ public/scripts/view_a_map.js
@@ -19,6 +19,14 @@ $(() => {
     }
 
   })
+  .then(data => {
+    const map = L.map('map').setView([50.445210, -104.618896], 13)
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+  })
 
 
 })

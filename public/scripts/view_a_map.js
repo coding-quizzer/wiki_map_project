ac43ef3124599a5ffb250e9b6cae77190f81a48c
Vinson Wong <vinson.wong93@gmail.com>
2022-10-25 13:20:21 -0400

    add css style to all views page, implement fixed nav bar to all pages

--- public/scripts/view_a_map.js
+++ public/scripts/view_a_map.js
@@ -6,20 +6,20 @@ $(() => {
     method: 'GET',
     url: `/api/maps/${map_id}`
   })
-  .then(response => {
-    const map = response.map;
-    $mapTitle.text(map.name);
-    return $.get(`/api/maps/${map_id}/points`);
-  })
-  .then(data => {
-    const points = data.points;
-    console.log(data);
-    const $pointList = $('#points');
-    for(const point of points) {
-      $('<li class="point">').text(point.title).appendTo($pointList);
-    }
+    .then(response => {
+      const map = response.map;
+      $mapTitle.text(map.name);
+      return $.get(`/api/maps/${map_id}/points`);
+    })
+    .then(data => {
+      const points = data.points;
+      console.log(data);
+      const $pointList = $('#points');
+      for (const point of points) {
+        $('<li class="point">').text(point.title).appendTo($pointList);
+      }
 
-  })
+    });
 
 
-})
+});
--- public/scripts/view_maps.js
+++ public/scripts/view_maps.js
@@ -4,33 +4,33 @@ $(() => {
     method: 'GET',
     url: `/api/maps`
   })
-  .then((response) => {
-    const maps = response.maps;
-    // console.log(maps);
-    for (let map of maps) {
+    .then((response) => {
+      const maps = response.maps;
+      // console.log(maps);
+      for (let map of maps) {
 
-      const $mapItem = $(`
+        const $mapItem = $(`
       <article class="map-item">
       <header>
       <img class="img_url">
       <h2 class="map-name"></h2>
-      <i class="fa-regular fa-star"></i>
+      <p class="createdBy">Created by: Username</p>
       </header>
       <footer>
-      <p class="createdBy">Created by: Username</p>
+      <i class="fa-solid fa-star"></i>
       </footer>
       </article>
       `);
 
-      const $img = $mapItem.find('img').attr('src', map.img_url);
-      const $mapName = $mapItem.find('.map-name').text(map.name);
-      const $createdBy = $mapItem.find('.createdBy').text('Created by: ' + map.username);
+        const $img = $mapItem.find('img').attr('src', map.img_url);
+        const $mapName = $mapItem.find('.map-name').text(map.name);
+        const $createdBy = $mapItem.find('.createdBy').text('Created by: ' + map.username);
 
-      $mapItem.appendTo('#maps-container');
+        $mapItem.appendTo('#maps-container');
 
-    }
+      }
 
-  });
+    });
 
 
 });

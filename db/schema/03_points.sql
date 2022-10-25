e50436f384129ac7b9d2e2e0348edcf4707878df
William Gadd <wgadd@sasktel.net>
2022-10-25 19:47:56 +0000

    Add grey square as default image for points

--- db/schema/03_points.sql
+++ db/schema/03_points.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS points CASCADE;
 CREATE TABLE points (
   id SERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(255) NOT NULL,
-  img_url VARCHAR(255) NOT NULL,
+  img_url VARCHAR(255) NOT NULL DEFAULT '/images/gray-square-hi.png',
   description TEXT,
   map_id INTEGER REFERENCES maps(id),
   latitude REAL NOT NULL,
--- db/seeds/03_points.sql
+++ db/seeds/03_points.sql
@@ -1,8 +1,8 @@
-INSERT INTO points (title, img_url, description, map_id, latitude, longitude)
-VALUES ('Tim Hortons', 'IMG', 'TIMMMIES', 1, 50.482456, -104.615591),
-('Starbucks', 'IMG', 'Go to Coffee place', 1, 43.856367, -79.337671),
-('Corner Cafe', 'IMG', 'Chill Vibes', 1, 50.458747, -104.648633),
-('Baskin Robbins', 'IMG', 'YUMYUM', 2, 49.341434, -123.156975),
-('ABC Theatres', 'IMG', 'IMAX Theatre', 3, 47.544195, -52.743841),
-('AMC Movie Theatres', 'IMG', 'AMC AMC AMC', 3, 42.983228, -81.24337),
-('Costco', 'IMG', 'Go to groceries', 4, 46.817119, -71.277264);
+INSERT INTO points (title, description, map_id, latitude, longitude)
+VALUES ('Tim Hortons', 'TIMMMIES', 1, 50.482456, -104.615591),
+('Starbucks', 'Go to Coffee place', 1, 43.856367, -79.337671),
+('Corner Cafe', 'Chill Vibes', 1, 50.458747, -104.648633),
+('Baskin Robbins', 'YUMYUM', 2, 49.341434, -123.156975),
+('ABC Theatres', 'IMAX Theatre', 3, 47.544195, -52.743841),
+('AMC Movie Theatres', 'AMC AMC AMC', 3, 42.983228, -81.24337),
+('Costco', 'Go to groceries', 4, 46.817119, -71.277264);
